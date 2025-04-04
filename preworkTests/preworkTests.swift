af404e4443b8413475d33faa734cf2f4d0cd0e00
marcanjoul <marc.anjoul@gmail.com>
2025-04-03 22:15:20 -0400

    added prework files to repository

---
+++ preworkTests/preworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  preworkTests.swift
+//  preworkTests
+//
+//  Created by marc  on 4/3/25.
+//
+
+import Testing
+@testable import prework
+
+struct preworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
